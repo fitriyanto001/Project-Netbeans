4a1baea1cb931ea25447180afb838371c7a78795
fitriyanto001 <fitriyanto25112003@gmail.com>
2026-04-09 20:09:03 +0800

    Upload project NetBeans Mahasiswa dan MataKuliah
    Project Java berisi class Mahasiswa dan MataKuliah sesuai tugas PBO

---
+++ Project_Netbeans/src/main/java/com/mycompany/project_netbeans/Mahasiswa.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.project_netbeans;
+
+/**
+ *
+ * @author FiTo
+ */
+public class Mahasiswa {
+    public static String Npm = "2410010008";
+    public static String namaLengkap = "Fitriyanto";
+    private static double ipk = 2.93;
+    public static int semester = 4;
+    
+    public static double getIpk() {
+        return ipk;
+    }
+    
+}
